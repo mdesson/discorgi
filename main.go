ca4e7d056ef84a16068d3c185c3311c40d1294a3
Michael Desson <michael.desson@gmail.com>
2020-03-20 01:01:19 -0400

    Added simple ping pong game to discorgi

--- main.go
+++ main.go
@@ -5,17 +5,51 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
+	// Fetch config
 	config, err := getConfig("config.json")
+	if err != nil {
+		fmt.Println("Error opening config.json:", err)
+		return
+	}
+
+	// Create discord session
 	discord, err := discordgo.New("Bot " + config["bot-token"])
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error creating discord session:", err)
+		return
 	}
-	fmt.Println(discord)
+
+	// Register handler
+	discord.AddHandler(messageCreate)
+
+	// Begin listening
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("Error opening connection: ", err)
+		return
+	}
+
+	// Close at end of session
+	defer discord.Close()
+
+	// Wait for SIGNAL to end program
+	fmt.Println("Discorgi is patiently listening. Press CTRL-C to exit")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+
+	select {
+	case <-sc:
+		fmt.Println("\nExiting...")
+
+	}
 }
 
 func getConfig(path string) (map[string]string, error) {
@@ -30,4 +64,22 @@ func getConfig(path string) (map[string]string, error) {
 	json.Unmarshal([]byte(jsonBytes), &config)
 
 	return config, nil
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	fmt.Println("Woof! Message received!")
+
+	// Ignore bot's messages
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// If message is ping, reply with pong
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
